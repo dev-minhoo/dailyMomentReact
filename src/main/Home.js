40e8b4b94a1e13f0ae798dc6f2f5de2b0a705e7f
dev-minhoo <minhoo920@naver.com>
2022-10-05 18:04:11 +0900

    dailyMomentReact start

---
+++ src/main/Home.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        hello test
+      </header>
+    </div>
+  );
+}
+
+export default Home;
